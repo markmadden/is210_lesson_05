eba36a061e4fb5017b6b4d8e4c6868b9b1dd1aad
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-29 01:30:40 -0400

    typos

--- task_03.py
+++ task_03.py
@@ -3,46 +3,46 @@
 """Converts temperatures from one unit of measure to another."""
 
 
-def celsius_to_farenheit(temperature):
-    """Converts degrees celsius to degrees farenheit.
+def celsius_to_fahrenheit(temperature):
+    """Converts degrees celsius to degrees fahrenheit.
 
         Args:
             temperature (num): temperature in degrees celsius
 
         Returns:
-            a numeric type of the temperature in degrees farenheit.
+            a numeric type of the temperature in degrees fahrenheit.
 
         Examples:
-            >>> celsius_to_farenheit(42)
+            >>> celsius_to_fahrenheit(42)
             107.6
     """
 
-    f = float((9 * temperature)/5 + 32)
-    return f
+    f = (9 * temperature)/5 + 32.0
+    return float(f)
 
-def farenheit_to_celsius(temperature):
-    """Converts degrees farenheit to degrees celsius.
+def fahrenheit_to_celsius(temperature):
+    """Converts degrees fahrenheit to degrees celsius.
 
         Args:
-            temperature (num): temperature in degrees farenheit
+            temperature (num): temperature in degrees fahrenheit
 
         Returns:
             a numeric type of the temperature in degrees celsius.
 
         Examples:
-            >>> farenheit_to_celsius(42)
+            >>> fahrenheit_to_celsius(42)
             5.0
     """
 
-    c = float((5 * (temperature - 32))/9)
-    return c
+    c = (5 * (temperature - 32))/9.0
+    return float(c)
 
 def convert_temperature(temperature, output_format='c'):
     """Detects type of temperature and outputs it as specified output type.
 
         Args:
             temperature (str): In the format of 35C or 45F with the
-            Celsius or Farenheit symbol
+            Celsius or Fahrenheit symbol
             output_format (str): Accepts "c" or "f" as valid input;
             defaults to 'c'
 
@@ -55,11 +55,10 @@ def convert_temperature(temperature, output_format='c'):
             
     """
     temperature = temperature[0:-1]
-    temperature = float(temperature)
     if output_format is 'c':
         result = farenheit_to_celsius(temperature)
     elif output_format is 'f':
-        result = celsius_to_farenheit(temperature)
+        result = celsius_to_fahrenheit(temperature)
     else:
         result = None
 
