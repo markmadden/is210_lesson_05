705aa7de395f65a06aed373413fe2931edf27558
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-28 20:36:38 -0400

    My code for task 01.

--- task_01.py
+++ task_01.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 """Task 01 Code."""
 
-def bool_to_str(bvalue, short = False):
+def bool_to_str(bvalue, short=False):
     """Converts a boolean to a string (short or long).
 
     Args:
         bvalue(bool): A boolen that is converted to a string.
         short(bool), optional): If False, bvalue will return as a whole
             word ('Yes' or 'No') else a single letter ('Y', 'N').
-            
+
     Returns:
         string: 'Yes' if bvalue is True or 'No' if bvalue is False.
 
@@ -22,7 +22,7 @@ def bool_to_str(bvalue, short = False):
         'No'
 
         >>>bool_to_str(False, short = True)
-        'N'    
+        'N'
     """
     if bvalue is True:
         return 'Yes' if short is False else 'Y'
