216334a9779b2f5f502519b0721fe7c65f4c7be9
markmadden <mark.madden@spsmail.cuny.edu>
2014-10-06 00:23:08 -0400

    task1

---
+++ task_01.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+""" Task1 of week 5"""
+
+def bool_to_str(bvalue, short=False):
+    
+    """Gives the string value of True or False
+
+    Arguments:
+        bvalue (boolean): value to be converted
+        short (boolean, optional): determines whether to return a
+            short value.  Default value is False.
+
+    Returns:
+       string: Yes/Y or No/N which is the converted boolean value
+
+    Examples:
+        >>> bool_to_str(True)
+        'Yes'
+
+        >>> bool_to_str(False)
+        'No'
+
+        >>> bool_to_str(bvalue=True, short=True)
+        'Y'
+    """
+
+    if bvalue:
+        value = 'Yes'
+    else:
+        value = 'No'
+
+    if short:
+        value = value[0]
+
+    return value
